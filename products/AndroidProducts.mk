26302e6e3f5fa53ef12e0e161ced91df81a4e4fe
Drew Walton <drewwalton19216801@gmail.com>
2012-08-24 16:31:15 -0600

    Add HTC Incredible

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -6,6 +6,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_thunderc_MS690.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_LS670.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_VS660.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_VM670.mk
+    $(LOCAL_DIR)/cyanogen_thunderc_VM670.mk \
+    $(LOCAL_DIR)/cyanogen_inc.mk
 
 
